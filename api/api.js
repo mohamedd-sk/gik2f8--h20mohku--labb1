3110a62482642c0688dce06a46d9794c4959b93d
Mohamed Sheikhdon <h20mohku>
2022-12-08 16:36:16 +0000

    har lagt till reeadme.md

---
+++ api/api.js
@@ -0,0 +1,10 @@
+const url = 'https://gik2f8-labs.herokuapp.com/books';
+
+async function getAll() {
+  const result = await fetch(url)
+    .then((result) => result.json())
+    .catch((e) => e);
+
+  return result;
+ 
+}
